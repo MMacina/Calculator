01f882a9f00d5f3b6e68b8adc045b3ac5197eb09
Marcin <marcin.macina@outlook.com>
2022-07-07 21:49:49 +0200

    Calculator commit

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,4 +1,20 @@
 package com.kodilla.calculator;
 
 public class Calculator {
+
+    public int adding(int a, int b){
+        return a + b;
+    }
+
+    public int subtracting(int a, int b){
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int addingResoult = calculator.adding(2, 3);
+        System.out.println("Adding resoult is " +  addingResoult);
+        int subtractingResoult = calculator.subtracting(2, 3);
+        System.out.println("Subtracting resoult is " +  subtractingResoult);
+    }
 }
